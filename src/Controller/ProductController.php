c7575d45053213a9728f57fd9c5776019fa2f621
Nathan JOLIVAT <nathan@enjoyup.io>
2018-10-09 16:12:33 +0200

    Add button to see specific detailed movie into movies list

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -5,47 +5,79 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\VarDumper\VarDumper;
 
 class ProductController extends AbstractController
 {
     /**
-     * @Route("/index", name="index")
+     * @Route("/specific", name="specific")
      */
-    public function index()
+    public function specific()
     {
         $omdbApiKey = getenv('OMDB_API_KEY');
-        $apiResponse = file_get_contents('http://www.omdbapi.com/?i=tt3896198&apikey=35510d96');
-        $jsonResponse = json_decode($apiResponse);
 
-        return $this->render('omdb/index.html.twig', [ 'omdbApiKey' => $omdbApiKey, 'jsonResponse' => $jsonResponse ]);
+        return $this->render('omdb/specific.html.twig', ['omdbApiKey' => $omdbApiKey]);
     }
 
     /**
-     * @Route("/find", name="find")
+     * @Route("/find-specific/{title}", name="find-specific")
+     * @param $title
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function getMovieTitle($title)
+    {
+        $omdbApiKey = getenv('OMDB_API_KEY');
+
+        $makeTitleUrl = "http://www.omdbapi.com/?apikey=" . getenv('OMDB_API_KEY') . "&t=" . urlencode($title);
+        $queryTitle = file_get_contents($makeTitleUrl);
+
+        $titleResponse = json_decode($queryTitle);
+
+        return $this->render('omdb/specific.html.twig', [
+            'responseMovie' => $titleResponse,
+            'omdbApiKey'    => $omdbApiKey,
+            'titleResponse' => $titleResponse !== null ? $titleResponse : null
+        ]);
+    }
+
+    /**
+     * @Route("/multiple", name="multiple")
+     */
+    public function multiple()
+    {
+        $omdbApiKey = getenv('OMDB_API_KEY');
+
+        return $this->render('omdb/multiple.html.twig', ['omdbApiKey' => $omdbApiKey]);
+    }
+
+    /**
+     * @Route("/find-multiple", name="find-multiple")
      * @param Request $request
      *
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function getMovieTitle(Request $request)
+    public function getMovieTitleMultiple(Request $request)
     {
         $omdbApiKey = getenv('OMDB_API_KEY');
 
         $requestedTitle = $request->query->get('movieTitle');
 
-        $makeUrl = "http://www.omdbapi.com/?apikey=" . getenv('OMDB_API_KEY') . "&t=". urlencode($requestedTitle);
+        $makeUrl = "http://www.omdbapi.com/?apikey=" . getenv('OMDB_API_KEY') . "&s=" . urlencode($requestedTitle);
 
         $query = file_get_contents($makeUrl);
 
         $response = json_decode($query);
 
-        return $this->render('omdb/index.html.twig', ['responseMovie' => $response, 'omdbApiKey' => $omdbApiKey]);
+        return $this->render('omdb/multiple.html.twig', [
+            'responseMovie' => $response,
+            'omdbApiKey'    => $omdbApiKey
+        ]);
     }
 
     public function givesApiKeyToView()
     {
         $omdbApiKey = getenv('OMDB_API_KEY');
 
-        return $this->render('omdb/layout/navbar.html.twig', [ 'omdbApiKey' => $omdbApiKey ]);
+        return $this->render('omdb/layout/navbar.html.twig', ['omdbApiKey' => $omdbApiKey]);
     }
 }
