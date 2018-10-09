7a0395ab6b2da87a2c34f8723723d6c161eb17b1
Nathan JOLIVAT <nathan@enjoyup.io>
2018-10-09 11:36:52 +0200

    Add process method and view to see returned response from OMDB Api

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\VarDumper\VarDumper;
 
 class ProductController extends AbstractController
 {
@@ -12,8 +14,38 @@ class ProductController extends AbstractController
      */
     public function index()
     {
-        getenv('OMDB_API_KEY');
-        
-        return $this->render('product/index.html.twig');
+        $omdbApiKey = getenv('OMDB_API_KEY');
+        $apiResponse = file_get_contents('http://www.omdbapi.com/?i=tt3896198&apikey=35510d96');
+        $jsonResponse = json_decode($apiResponse);
+
+        return $this->render('omdb/index.html.twig', [ 'omdbApiKey' => $omdbApiKey, 'jsonResponse' => $jsonResponse ]);
+    }
+
+    /**
+     * @Route("/find", name="find")
+     * @param Request $request
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function getMovieTitle(Request $request)
+    {
+        $omdbApiKey = getenv('OMDB_API_KEY');
+
+        $requestedTitle = $request->query->get('movieTitle');
+
+        $makeUrl = "http://www.omdbapi.com/?apikey=" . getenv('OMDB_API_KEY') . "&t=". urlencode($requestedTitle);
+
+        $query = file_get_contents($makeUrl);
+
+        $response = json_decode($query);
+
+        return $this->render('omdb/index.html.twig', ['responseMovie' => $response, 'omdbApiKey' => $omdbApiKey]);
+    }
+
+    public function givesApiKeyToView()
+    {
+        $omdbApiKey = getenv('OMDB_API_KEY');
+
+        return $this->render('omdb/layout/navbar.html.twig', [ 'omdbApiKey' => $omdbApiKey ]);
     }
 }
